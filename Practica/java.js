75f7bf7b56680b5763e7a1132694a8093abb9280
SanDpertt <100301861+SanDpertt@users.noreply.github.com>
2022-05-26 15:33:55 -0500

    Add files via upload

--- Practica/java.js
+++ Practica/java.js
@@ -1,4 +1,36 @@
-const form = document.getElementById("formulario")
+const form = document.getElementById("formulario");
+
 form.addEventListener("submit", function(event) {
-    event.preventDefault()
+    event.preventDefault();
+
+    let insertRowtable = new FormDatos(form);
+    insertRowtable(PreformDatos)
+
 })
+
+function insertRowtable(PreformDatos) {
+
+    let Table = document.getElementById("tablita");
+    let newLineRow = Table.insertRow(-1);
+    let newCelda = newLineRow.insertCell(0);
+
+    let n1 = 0;
+    let n2 = 0;
+    let options = "";
+    let Texto = "";
+
+    let indice = document.formul.miSelect.selectedIndex
+    let valor = document.formul.miSelect.options[indice].value
+
+    n1 = document.getElementById("n1").value;
+    n2 = document.getElementById("n2").value;
+
+    newCelda.textContent = n1;
+
+    let newCelda2 = newLineRow.insertCell(1);
+    newCelda2.textContent = n2;
+
+    let newCelda3 = newLineRow.insertCell(2);
+    newCelda3.textContent = valor;
+
+}
